a43f7c34875732a03bde4f4e8701898af04287db
张驰阳 <zhangchiyang@sfmail.sf-express.com>
2023-12-21 23:54:16 +0800

    update

--- src/pages/Index/Modules/SwiperImg.tsx
+++ src/pages/Index/Modules/SwiperImg.tsx
@@ -2,7 +2,7 @@
  * @Author: 张驰阳 zhangchiyang@sfmail.sf-express.com
  * @Date: 2023-07-29 23:08:59
  * @LastEditors: 张驰阳 zhangchiyang@sfmail.sf-express.com
- * @LastEditTime: 2023-12-03 22:17:32
+ * @LastEditTime: 2023-12-21 23:21:57
  * @FilePath: /zulinv2/src/pages/Index/Modules/SwiperImg.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -37,14 +37,16 @@ const SwiperImg = (props) => {
         let cid = e.currentTarget.dataset.cid;
         let gid = e.currentTarget.dataset.gid;
         let title = e.currentTarget.dataset.title;
+        let cardid = e.currentTarget.dataset.cardid;
         dumpByType(gotype, {
             title: title,
             gid: gid,
             cid: cid,
             eid: eid,
+            cardid: cardid,
         })
     }
-
+    console.log('props.datas', props.datas);
     return (
         <View>
                 <Swiper
@@ -62,6 +64,7 @@ const SwiperImg = (props) => {
                                 data-eid={item.goods[0].id?item.goods[0].id:''}
                                 data-cid={item.goods[0].id?item.goods[0].id:''}
                                 data-gid={item.goods[0].id?item.goods[0].id:''}
+                                data-cardid={item.cards[0].id? item.cards[0].id:''}
                                 onClick={dump}
                             >
                                 <Image className='index-swiper-img' src={item.fpath} mode="widthFix" onLoad={computeImgHeight} />
